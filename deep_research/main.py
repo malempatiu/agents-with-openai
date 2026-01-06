434c539599631b1d07eaaf18559078c1d4d27876
ugesh malempati <malempatiu@gmail.com>
2026-01-06 10:52:59 +0100

    Implement research manager and visualize the flow in gradio ui

--- deep_research/main.py
+++ deep_research/main.py
@@ -1,15 +1,22 @@
-from config import check_openai_api_key
-from asyncio import run
+import gradio as gr
+from dotenv import load_dotenv
+from research_manager import ResearchManager
+
+load_dotenv(override=True)
 
 
-def do_app_checks():
-    check_openai_api_key()
+async def run(query: str):
+    async for chunk in ResearchManager().run(query):
+        yield chunk
 
 
-async def main():
-    await do_app_checks()
-    
+with gr.Blocks(theme=gr.themes.Default(primary_hue="sky")) as ui:
+    gr.Markdown("# Deep Research")
+    query_textbox = gr.Textbox(label="What topic would you like to research?")
+    run_button = gr.Button("Run", variant="primary")
+    report = gr.Markdown(label="Report")
 
+    run_button.click(fn=run, inputs=query_textbox, outputs=report)
+    query_textbox.submit(fn=run, inputs=query_textbox, outputs=report)
 
-if __name__ == '__main__':
-    run(main())
+ui.launch(inbrowser=True)
